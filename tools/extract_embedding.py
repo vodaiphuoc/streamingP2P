81d5370c2046ddb4389ef1410a89db72c0f5905e
vodaiphuoc <phuocvodn98@gmail.com>
2026-01-14 18:41:36 +0700

    formatting

--- tools/extract_embedding.py
+++ tools/extract_embedding.py
@@ -37,13 +37,14 @@ def single_job(utt):
 def main(args):
     all_task = [executor.submit(single_job, utt) for utt in utt2wav.keys()]
     utt2embedding, spk2embedding = {}, {}
-    for future in tqdm(as_completed(all_task)):
+    for future in tqdm(as_completed(all_task), total=len(all_task)):
         utt, embedding = future.result()
         utt2embedding[utt] = embedding
         spk = utt2spk[utt]
         if spk not in spk2embedding:
             spk2embedding[spk] = []
         spk2embedding[spk].append(embedding)
+
     for k, v in spk2embedding.items():
         spk2embedding[k] = torch.tensor(v).mean(dim=0).tolist()
     torch.save(utt2embedding, "{}/utt2embedding.pt".format(args.dir))
