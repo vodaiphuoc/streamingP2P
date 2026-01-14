da54502ad1c25d1f72c3c9b3e21f5f95893e316a
vodaiphuoc <phuocvodn98@gmail.com>
2026-01-15 03:18:37 +0700

    fix num gpus

--- examples/libritts/cosyvoice2/run.sh
+++ examples/libritts/cosyvoice2/run.sh
@@ -57,7 +57,7 @@ fi
 
 # Stage 5: Training
 # (Update the 'cat' commands to point to your new folders)
-export CUDA_VISIBLE_DEVICES="0,1,2,3"
+export CUDA_VISIBLE_DEVICES="0,1"
 num_gpus=$(echo $CUDA_VISIBLE_DEVICES | awk -F "," '{print NF}')
 job_id=1986
 dist_backend="nccl"
